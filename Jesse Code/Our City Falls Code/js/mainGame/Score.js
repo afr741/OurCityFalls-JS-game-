054fd6560cc4c489cf905dcea4eedcf405c2fab3
jdaDev <jesse.d.alexander22@gmail.com>
2018-04-15 19:53:42 -0230

    Update Score.js

--- Jesse Code/Our City Falls Code/js/mainGame/Score.js
+++ Jesse Code/Our City Falls Code/js/mainGame/Score.js
@@ -1,88 +1,88 @@
 function Score() {
-    var view = View.getInstance();
-  
-    var mainWrapper;
-    var scoreWrapper;
-    /*var coinScoreWrapper;*/
-    var totalScoreWrapper;
-    var lifeCountWrapper;
-    var levelWrapper;
-  
-    /*this.coinScore;*/
-    this.totalScore;
-    this.lifeCount;
-  
-    var that = this;
-  
-    this.init = function() {
-      /*that.coinScore = 0;*/
-      that.totalScore = 0;
-      that.lifeCount = 5;
-  
-      mainWrapper = view.getMainWrapper();
-  
-      scoreWrapper = view.create('div');
-      /*coinScoreWrapper = view.create('div');*/
-      totalScoreWrapper = view.create('div');
-      lifeCountWrapper = view.create('div');
-      levelWrapper = view.create('div');
-  
-      view.addClass(scoreWrapper, 'hudWrapper');
-      /*view.addClass(coinScoreWrapper, 'coin-score');*/
-      view.addClass(totalScoreWrapper, 'total-score');
-      view.addClass(lifeCountWrapper, 'lives');
-      view.addClass(levelWrapper, 'level-num');
-  
-      view.append(scoreWrapper, levelWrapper);
-      view.append(scoreWrapper, lifeCountWrapper);
-      /*view.append(scoreWrapper, coinScoreWrapper);*/
-      view.append(scoreWrapper, totalScoreWrapper);
-      view.append(mainWrapper, scoreWrapper);
-  
-      /*that.updateCoinScore();*/
-      that.updateTotalScore();
+  var gameDivs = GameDivs.getInstance();
+
+  var mainWrapper;
+  var scoreWrapper;
+  var coinScoreWrapper;
+  var totalScoreWrapper;
+  var lifeCountWrapper;
+  var levelWrapper;
+
+  this.coinScore;
+  this.totalScore;
+  this.lifeCount;
+
+  var that = this;
+
+  this.init = function() {
+    that.coinScore = 0;
+    that.totalScore = 0;
+    that.lifeCount = 5;
+
+    mainWrapper = gameDivs.getMainWrapper();
+
+    scoreWrapper = gameDivs.create('div');
+    coinScoreWrapper = gameDivs.create('div');
+    totalScoreWrapper = gameDivs.create('div');
+    lifeCountWrapper = gameDivs.create('div');
+    levelWrapper = gameDivs.create('div');
+
+    gameDivs.addClass(scoreWrapper, 'hudWrapper');
+    gameDivs.addClass(coinScoreWrapper, 'coin-score');
+    gameDivs.addClass(totalScoreWrapper, 'total-score');
+    gameDivs.addClass(lifeCountWrapper, 'lives');
+    gameDivs.addClass(levelWrapper, 'level-num');
+
+    gameDivs.append(scoreWrapper, levelWrapper);
+    gameDivs.append(scoreWrapper, lifeCountWrapper);
+    gameDivs.append(scoreWrapper, coinScoreWrapper);
+    gameDivs.append(scoreWrapper, totalScoreWrapper);
+    gameDivs.append(mainWrapper, scoreWrapper);
+
+    that.updateCoinScore();
+    that.updateTotalScore();
+    that.updateLifeCount();
+    that.updateLevelNum(1);
+  };
+
+  this.updateCoinScore = function() {
+    if (that.coinScore == 100) {
+      that.coinScore = 0;
+      that.lifeCount++;
       that.updateLifeCount();
-      that.updateLevelNum(1);
-    };
-  
-    /*this.updateCoinScore = function() {
-      if (that.coinScore == 100) {
-        that.coinScore = 0;
-        that.lifeCount++;
-        that.updateLifeCount();
-      }*/
-  
-      /*view.setHTML(coinScoreWrapper, 'Coins: ' + that.coinScore);
-    };*/
-  
-    this.updateTotalScore = function() {
-      view.setHTML(totalScoreWrapper, 'Score: ' + that.totalScore);
-    };
-  
-    this.updateLifeCount = function() {
-      view.setHTML(lifeCountWrapper, 'x ' + that.lifeCount);
-    };
-  
-    this.updateLevelNum = function(level) {
-      view.setHTML(levelWrapper, 'Level: ' + level);
-    };
-  
-    this.displayScore = function() {
-      view.style(scoreWrapper, { display: 'block', background: '#330d00' });
-    };
-  
-    this.hideScore = function() {
-      view.style(scoreWrapper, { display: 'none' });
-  
-      /*that.coinScore = 0;*/
-      that.lifeCount = 5;
-      that.totalScore = 0;
-      /*that.updateCoinScore();*/
-      that.updateTotalScore();
-      that.updateLifeCount();
-    };
-  
-    this.gameOverView = function() {
-      view.style(scoreWrapper, { background: 'black' });
-    };
-  }
+    }
+
+    gameDivs.setHTML(coinScoreWrapper, 'Coins: ' + that.coinScore);
+  };
+
+  this.updateTotalScore = function() {
+    gameDivs.setHTML(totalScoreWrapper, 'Score: ' + that.totalScore);
+  };
+
+  this.updateLifeCount = function() {
+    gameDivs.setHTML(lifeCountWrapper, 'x ' + that.lifeCount);
+  };
+
+  this.updateLevelNum = function(level) {
+    gameDivs.setHTML(levelWrapper, 'Level: ' + level);
+  };
+
+  this.displayScore = function() {
+    gameDivs.style(scoreWrapper, { display: 'block', background: '#330d00' });
+  };
+
+  this.hideScore = function() {
+    gameDivs.style(scoreWrapper, { display: 'none' });
+
+    that.coinScore = 0;
+    that.lifeCount = 5;
+    that.totalScore = 0;
+    that.updateCoinScore();
+    that.updateTotalScore();
+    that.updateLifeCount();
+  };
+
+  this.gameOverView = function() {
+    gameDivs.style(scoreWrapper, { background: 'black' });
+  };
+}
